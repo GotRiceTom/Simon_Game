953ebeb3f91d3d804689397bc0f432e51d841d50
Eric Naegle <mr.naegle@gmail.com>
2018-10-19 12:24:50 -0600

    Commented Model

--- model.h
+++ model.h
@@ -6,10 +6,18 @@
 class Model
 {
 private:
+    //can either be "player", "AI", or "wait"
     std::string gameState;
+
+    //how many times the player has clicked
     int playerProgress;
+
+    //length of current sequence
     int level;
+
     int sequence[1000];
+
+    //constructor
     Model();
 
 public:
